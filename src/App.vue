24ea003599c99da34816115722b03a43fe59eb7f
eliezereoc <eliezeroc@gmail.com>
2021-09-06 09:01:32 -0300

    Intalação e configuração do primevue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Cálculo do IMC</h1>
+    <h2>Digito seu peso e altura para calcular o IMC</h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  export default {
+    
+    
   }
-}
 </script>
 
 <style>
@@ -19,8 +19,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
